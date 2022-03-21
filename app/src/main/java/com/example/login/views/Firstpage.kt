b89a1441265032053504ba63067763a43854a146
Subhash <sboss4807@gmail.com>
2022-03-21 22:54:09 +0530

    TextInputLayout added

--- app/src/main/java/com/example/login/views/Firstpage.kt
+++ app/src/main/java/com/example/login/views/Firstpage.kt
@@ -6,40 +6,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.login.R
+import com.google.android.material.textfield.TextInputLayout
 
 
 class Firstpage : Fragment() {
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.activity_firstpage, container, false)
         val login = view.findViewById<Button>(R.id.login_btn)
         val fragmentManager = parentFragmentManager
-        val username = view.findViewById<EditText>(R.id.user_input)
-        val password = view.findViewById<EditText>(R.id.pass_input)
+        val username = view.findViewById<TextInputLayout>(R.id.username_input)
+        val password = view.findViewById<TextInputLayout>(R.id.password_input)
         val bundle = Bundle()
 
         login.setOnClickListener {
-            val usernameOne = username.text.toString()
-            val passwordOne = password.text.toString()
-            if (usernameOne.isEmpty() || passwordOne.isEmpty()) {
-                Toast.makeText(activity, "Fill the required fields", Toast.LENGTH_LONG).show()
-            } else {
-                bundle.putString("Message", usernameOne)
-                val transaction = fragmentManager.beginTransaction()
-                val fTwo = Basicfrag()
-                fTwo.arguments = bundle
-                transaction.replace(R.id.landing_fragment, fTwo)
-                transaction.addToBackStack(null)
-                transaction.commit()
+            val usernameOne = username.editText?.text.toString().trim()
+            val passwordOne = password.editText?.text.toString().trim()
+            username.error = null
+            password.error = null
+            when {
+                usernameOne.isEmpty() -> {
+                    username.error = "This is an required field"
+                }
+                usernameOne.length > 15 -> {
+                    username.error = "Username is Too long"
+                }
+                passwordOne.isEmpty() -> {
+                    password.error = "This is an required field"
+                }
+                else -> {
+                    bundle.putString("Message", usernameOne)
+                    val transaction = fragmentManager.beginTransaction()
+                    val fTwo = Basicfrag()
+                    fTwo.arguments = bundle
+                    transaction.replace(R.id.landing_fragment, fTwo)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
             }
         }
         return view
     }
 }
+
